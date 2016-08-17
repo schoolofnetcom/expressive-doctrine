03d20bca3cf38a636aa094e0fb2dec92f56a973c
Luiz <argentinaluiz@gmail.com>
2016-08-17 09:54:28 -0300

    Primeiro commit

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                'driverClass' => \Doctrine\DBAL\Driver\PDOMySql\Driver::class,
+                'params' => [
+                    'host' => 'localhost',
+                    'port' => '3306',
+                    'user' => 'username',
+                    'password' => 'password',
+                    'dbname' => 'database',
+                    'driverOptions' => [
+                        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+                    ]
+                ]
+            ]
+        ],
+        'driver' => [
+            'App_driver' => [
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../../src/App/Entity']
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    'App\Entity' => 'App_driver'
+                ]
+            ]
+        ]
+    ]
+];
---
+++ src/App/Entity/Product.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $valueSale;
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $valueCost = 0;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity = 0;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValueSale()
+    {
+        return $this->valueSale;
+    }
+
+    /**
+     * @param mixed $valueSale
+     * @return Product
+     */
+    public function setValueSale($valueSale)
+    {
+        $this->valueSale = $valueSale;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValueCost()
+    {
+        return $this->valueCost;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+
+}
